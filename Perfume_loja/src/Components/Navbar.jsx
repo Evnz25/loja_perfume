dee3f6a8ad86b88f9076e46e2817b1d4fa876107
Evnz25 <evandronicolau55@gmail.com>
2025-11-11 00:42:46 -0300

    update dos arquivos

---
+++ Perfume_loja/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="d-flex justify-content-between py-3 px-5 bg-secondary text-white">
+      <a href="" className="navbar-brand fs-4 fw-bolder">
+        Shop
+      </a>
+    </div>
+  );
+};
+
+export default Navbar;
