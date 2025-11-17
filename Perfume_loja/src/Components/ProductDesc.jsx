714b824a8abe9c98349ae82a163c6fc34e302003
Evnz25 <evandronicolau55@gmail.com>
2025-11-17 18:48:26 -0300

    update

--- Perfume_loja/src/Components/ProductDesc.jsx
+++ Perfume_loja/src/Components/ProductDesc.jsx
@@ -1,20 +1,25 @@
 import React from "react";
+import './ProductDesc.css'
 
 function ProductDesc({ produto }) {
     return (
-
-        <div className="card mb-3" style={{ maxWidth: '540px' }}>
+        <div className="description">
+        <div className="card mb-3" style={{ maxWidth: '1000px' }}>
         <div className="row g-0">
             <div className="col-md-4">
             <img src={produto.imagem} className="img-fluid rounded-start"  />
             </div>
             <div className="col-md-8">
             <div className="card-body">
-                <h5 className="card-title">{produto.nome}</h5>
+                <h2 className="card-title">{produto.nome}</h2>
+                <h6 className="card-especificacao">{produto.especificacao}</h6>
+                <h6 className="card-fabricante">{produto.fabricante}</h6>
                 <p className="card-text">{produto.descricao}</p>
-                <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                <p className="card-text">{produto.preco}</p>
+                <a className="btn btn-primary" onClick={() => navigate (`/product/${produto.id}`)}>Comprar</a>
             </div>
             </div>
+        </div>
         </div>
         </div>
 
--- Perfume_loja/src/Pages/Productpage.jsx
+++ Perfume_loja/src/Pages/Productpage.jsx
@@ -1,6 +1,7 @@
+import axios from 'axios';
 import { useParams } from "react-router-dom";
 import Navbar from "../Components/Navbar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ProductDesc from "../Components/ProductDesc";
 import produtosData from "../../Produtos.json";
 
@@ -8,6 +9,13 @@ import produtosData from "../../Produtos.json";
 function Productpage () {
     const { id } = useParams();
     const produto = produtosData.produtos.find(p => p.id === Number(id))
+    const [endereco, setEndereco] = useState(null);
+
+    useEffect(() => {
+        axios.get(`viacep.com.br/ws/${cep}/json/ `)
+        .then(res => setEndereco(res.data))
+        .catch(err => console.error("Erro em buscar cep (Productpage)", err))
+    }, [cep]);
 
     return (
         <>
