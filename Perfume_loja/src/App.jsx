40c1532e68eff8a34f4c9e377670e45d0a9ae654
Evnz25 <evandronicolau55@gmail.com>
2025-11-16 22:37:14 -0300

    update

--- Perfume_loja/src/App.jsx
+++ Perfume_loja/src/App.jsx
@@ -1,16 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/Navbar";
-import produtos from "../Produtos.json"
-import ProductCard from "./Components/ProductCard"
+import produtos from "../Produtos.json";
+import ProductCard from "./Components/ProductCard";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div>
-        {produtos.produtos.map((produtos) => (<ProductCard
-        key={produtos.id}
-        produto={produtos}/>))}
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        {produtos.produtos.map((produto) => (
+          <div class="col" key={produto.id}>
+            <ProductCard key={produto.id} produto={produto} />
+          </div>
+        ))}
       </div>
     </>
   );
--- Perfume_loja/src/Components/ProductCard.jsx
+++ Perfume_loja/src/Components/ProductCard.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-function ProductCard({produto}){
+function ProductCard({ produto }) {
   return (
-  <div className="card" style={{width: '18rem;'}}>
-    <img src={produto.imagem} className="card-img-top" alt="..."  style={{ height: '200px', objectFit: 'cover' }}/>
-    <div className="card-body">
-      <p className="card-text">{produto.nome}</p>
+    <div class="card h-100 ">
+      <img src={produto.imagem} class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">{produto.nome}</h5>
+        <p class="card-text">
+          This is a longer card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
     </div>
-  </div>
   );
-};
+}
 
-export default ProductCard
+export default ProductCard;
