b39dcbbf952f7c311c80063e9a13daac72be2141
Evnz25 <evandronicolau55@gmail.com>
2025-11-17 22:23:53 -0300

    update

--- Perfume_loja/src/Components/ProductDesc.jsx
+++ Perfume_loja/src/Components/ProductDesc.jsx
@@ -1,29 +1,78 @@
 import React from "react";
-import './ProductDesc.css'
+import axios from "axios";
+import { useState } from "react";
+import "./ProductDesc.css";
 
 function ProductDesc({ produto }) {
-    return (
-        <div className="description">
-        <div className="card mb-3" style={{ maxWidth: '1000px' }}>
-        <div className="row g-0">
-            <div className="col-md-4">
-            <img src={produto.imagem} className="img-fluid rounded-start"  />
-            </div>
-            <div className="col-md-8">
-            <div className="card-body">
-                <h2 className="card-title">{produto.nome}</h2>
-                <h6 className="card-especificacao">{produto.especificacao}</h6>
-                <h6 className="card-fabricante">{produto.fabricante}</h6>
-                <p className="card-text">{produto.descricao}</p>
-                <p className="card-text">{produto.preco}</p>
-                <a className="btn btn-primary" onClick={() => navigate (`/product/${produto.id}`)}>Comprar</a>
-            </div>
-            </div>
-        </div>
-        </div>
-        </div>
+  const [cepInput, setCepInput] = useState("");
+  const [endereco, setEndereco] = useState(null);
 
-);
+  const handleBuscaCep = () => {
+    setEndereco(null);
+
+    axios
+      .get(`https://viacep.com.br/ws/${cepInput}/json/`)
+      .then((res) => {
+        setEndereco(res.data);
+      })
+      .catch((err) => {
+        console.error("Erro ao buscar o CEP (Productpage): ", err);
+      });
+  };
+
+  return (
+    <div className="description">
+      <div className="card mb-3" style={{ maxWidth: "1000px" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src={produto.imagem} className="img-fluid rounded-start" />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h2 className="card-title">{produto.nome}</h2>
+              <h6 className="card-especificacao">{produto.especificacao}</h6>
+              <h6 className="card-fabricante">{produto.fabricante}</h6>
+              <p className="card-text">{produto.descricao}</p>
+              <p className="card-text">{produto.preco}</p>
+              <a
+                className="btn btn-primary"
+                onClick={() => navigate(`/product/${produto.id}`)}
+              >
+                Comprar
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Seu CEP (só números)"
+            value={cepInput} // O valor vem do estado
+            onChange={(e) => setCepInput(e.target.value)} // O estado é atualizado a cada dígito
+          />
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={handleBuscaCep}
+          >
+            Calcular cep
+          </button>
+        </div>
+        {endereco && (
+          <div className="alert alert-success">
+            <p className="mb-0">
+              <strong>Endereço:</strong> {endereco.logradouro},{" "}
+              {endereco.bairro}
+            </p>
+            <p className="mb-0">
+              <strong>Cidade:</strong> {endereco.localidade} - {endereco.uf}
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default ProductDesc;
--- Perfume_loja/src/Pages/Productpage.jsx
+++ Perfume_loja/src/Pages/Productpage.jsx
@@ -1,28 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 import { useParams } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 import { useEffect, useState } from "react";
 import ProductDesc from "../Components/ProductDesc";
 import produtosData from "../../Produtos.json";
 
+function Productpage() {
+  const { id } = useParams();
+  const produto = produtosData.produtos.find((p) => p.id === Number(id));
 
-function Productpage () {
-    const { id } = useParams();
-    const produto = produtosData.produtos.find(p => p.id === Number(id))
-    const [endereco, setEndereco] = useState(null);
-
-    useEffect(() => {
-        axios.get(`viacep.com.br/ws/${cep}/json/ `)
-        .then(res => setEndereco(res.data))
-        .catch(err => console.error("Erro em buscar cep (Productpage)", err))
-    }, [cep]);
-
-    return (
-        <>
-        <Navbar />
-        <ProductDesc key={produto.id} produto={produto} />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <ProductDesc key={produto.id} produto={produto} />
+    </>
+  );
 }
 
-export default Productpage
+export default Productpage;
